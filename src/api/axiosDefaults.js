cabebc555e94f66d3f22186cb9af3a191279c6b0
Madu-J Omenka <madu.omenka@hotmail.com>
2024-04-02 13:47:21 +0000

    Adjusted NavBar

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://django-restf.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
