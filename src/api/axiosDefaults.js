04924302741985f4000194efa4913b7dd6db3aeb
Madu Omenka <madu.omenka@hotmail.com>
2024-08-26 19:22:36 +0000

    Fixed cors issue

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://django-restf.herokuapp.com/';
+axios.defaults.baseURL = 'https://django-restf-79ed4265cb44.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.withCredentials = true;
 
